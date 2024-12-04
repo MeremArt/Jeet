c8bbedd2aa7e1fd8037178093e6c40925890c3f4
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-12-04 07:40:43 +0100

    fixed

--- app/wallet/AppWalletProvider.tsx
+++ app/wallet/AppWalletProvider.tsx
@@ -6,14 +6,13 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
 import {
   PhantomWalletAdapter,
   SolflareWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
-
 import { BackpackWalletAdapter } from "@solana/wallet-adapter-backpack";
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function AppWalletProvider({
@@ -23,9 +22,9 @@ export default function AppWalletProvider({
 }) {
   const network = WalletAdapterNetwork.Mainnet;
 
-  // Use the environment variable for the RPC URL
+  // Use clusterApiUrl as fallback if env variable is not set
   const endpoint = useMemo(
-    () => process.env.NEXT_PUBLIC_SOLANA_RPC_URL || "",
+    () => process.env.NEXT_PUBLIC_SOLANA_RPC_URL || clusterApiUrl(network),
     [network]
   );
 
@@ -40,7 +39,7 @@ export default function AppWalletProvider({
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets}>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
