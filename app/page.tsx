17a88d96c7ef9530d7c3419e53e5167a66d3fae8
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-12-19 01:26:46 +0100

    header footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
-
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
